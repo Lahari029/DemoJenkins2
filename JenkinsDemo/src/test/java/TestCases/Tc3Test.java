001e621131a9e22dbce8f2ceed9bd2a536f556e7
lokeshnagineni98 <lokeshnagineni98@gmail.com>
2023-11-21 13:12:55 +0530

    FirstCommit

---
+++ JenkinsDemo/src/test/java/TestCases/Tc3Test.java
@@ -0,0 +1,15 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc3Test {
+	@Test
+	public void tc3()
+	{
+		Reporter.log("Tc3 Running", true);
+	}
+
+
+}
